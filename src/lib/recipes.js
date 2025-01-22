935150a10a097ba1b2a4b75595b8044543bf9edc
manudiass03 <emanuelle.santosdias03@gmail.com>
2025-01-22 17:44:29 -0400

    recipes

---
+++ src/lib/recipes.js
@@ -0,0 +1,176 @@
+// src/lib/recipes.js
+export const recipes = [
+    {
+      title: 'Bolo de Chocolate',
+      image: '/images/bolo-chocolate.jpg',
+      description: 'Receita deliciosa de bolo de chocolate com cobertura cremosa.',
+      ingredients: [
+        '2 xícaras de farinha de trigo',
+        '1 xícara de açúcar',
+        '1 xícara de achocolatado em pó',
+        '3 ovos',
+        '1 xícara de leite',
+        '1/2 xícara de óleo',
+        '1 colher de fermento em pó'
+      ],
+      instructions: [
+        'Preaqueça o forno a 180°C.',
+        'Misture os ingredientes secos.',
+        'Adicione os ingredientes líquidos e bata bem.',
+        'Asse por 40 minutos ou até que o bolo fique dourado.'
+      ]
+    },
+    {
+      title: 'Salada de Frutas',
+      image: '/images/salada-frutas.jpg',
+      description: 'Uma salada de frutas frescas, perfeita para o verão.',
+      ingredients: [
+        '1 maçã',
+        '1 banana',
+        '1 laranja',
+        '1 kiwi',
+        '1/2 melancia'
+      ],
+      instructions: [
+        'Corte as frutas em cubos.',
+        'Misture todas as frutas em uma tigela.',
+        'Sirva imediatamente.'
+      ]
+    },
+    {
+      title: 'Macarrão à Carbonara',
+      image: '/images/macarrao-carbonara.jpg',
+      description: 'Macarrão cremoso com bacon, ovos e queijo.',
+      ingredients: [
+        '250g de macarrão',
+        '150g de bacon',
+        '3 ovos',
+        '100g de queijo parmesão',
+        'Sal e pimenta a gosto'
+      ],
+      instructions: [
+        'Cozinhe o macarrão conforme as instruções do pacote.',
+        'Fria o bacon até ficar crocante.',
+        'Bata os ovos com queijo parmesão.',
+        'Misture tudo, adicione o bacon e tempere.'
+      ]
+    },
+    {
+      title: 'Sopa de Abóbora',
+      image: '/images/sopa-abobora.jpg',
+      description: 'Uma sopa cremosa e reconfortante de abóbora.',
+      ingredients: [
+        '500g de abóbora',
+        '1 cebola',
+        '2 dentes de alho',
+        '500ml de caldo de legumes',
+        'Sal e pimenta a gosto'
+      ],
+      instructions: [
+        'Cozinhe a abóbora até ficar macia.',
+        'Refogue cebola e alho.',
+        'Bata tudo no liquidificador com o caldo de legumes.'
+      ]
+    },
+    {
+      title: 'Panquecas Americanas',
+      image: '/images/panqueca-americana.jpg',
+      description: 'Panquecas fofinhas e deliciosas, perfeitas para o café da manhã.',
+      ingredients: [
+        '1 xícara de farinha de trigo',
+        '1 colher de fermento em pó',
+        '1 colher de açúcar',
+        '1 ovo',
+        '1 xícara de leite'
+      ],
+      instructions: [
+        'Misture todos os ingredientes até formar uma massa homogênea.',
+        'Aqueça uma frigideira e coloque pequenas porções de massa.',
+        'Frite por 2-3 minutos de cada lado.'
+      ]
+    },
+    {
+      title: 'Frango à Parmegiana',
+      image: '/images/frango-parmegiana.jpg',
+      description: 'Frango empanado com molho de tomate e queijo derretido.',
+      ingredients: [
+        '4 filés de peito de frango',
+        '1 ovo',
+        '1 xícara de farinha de rosca',
+        '100g de queijo mussarela',
+        'Molho de tomate'
+      ],
+      instructions: [
+        'Empane os filés de frango com ovo e farinha de rosca.',
+        'Frite os filés até ficarem dourados.',
+        'Cubra com molho de tomate e queijo, e leve ao forno até o queijo derreter.'
+      ]
+    },
+    {
+      title: 'Arroz à Grega',
+      image: '/images/arroz-grega.jpg',
+      description: 'Arroz saboroso com legumes e passas.',
+      ingredients: [
+        '2 xícaras de arroz',
+        '1 cenoura',
+        '1/2 xícara de ervilhas',
+        '1/4 de xícara de passas',
+        '1/2 cebola'
+      ],
+      instructions: [
+        'Cozinhe o arroz.',
+        'Refogue os legumes e adicione ao arroz pronto.',
+        'Misture as passas antes de servir.'
+      ]
+    },
+    {
+      title: 'Quiche de Alho-poró',
+      image: '/images/quiche-alho-poro.jpg',
+      description: 'Uma quiche saborosa com alho-poró e queijo.',
+      ingredients: [
+        '1 massa de torta pronta',
+        '2 alho-porós',
+        '200g de queijo',
+        '3 ovos',
+        '1 xícara de creme de leite'
+      ],
+      instructions: [
+        'Refogue o alho-poró.',
+        'Misture ovos, creme de leite e queijo.',
+        'Asse a quiche até dourar.'
+      ]
+    },
+    {
+      title: 'Torta de Maçã',
+      image: '/images/torta-maca.jpg',
+      description: 'Torta caseira de maçã com canela.',
+      ingredients: [
+        '2 maçãs',
+        '1 massa folhada',
+        '1 colher de canela em pó',
+        '1/4 de xícara de açúcar'
+      ],
+      instructions: [
+        'Corte as maçãs e adicione canela e açúcar.',
+        'Envolva as maçãs na massa folhada.',
+        'Asse por 30 minutos.'
+      ]
+    },
+    {
+      title: 'Brigadeiro',
+      image: '/images/brigadeiro.jpg',
+      description: 'Doce típico brasileiro de leite condensado e chocolate.',
+      ingredients: [
+        '1 lata de leite condensado',
+        '2 colheres de sopa de achocolatado',
+        '1 colher de manteiga',
+        'Chocolate granulado'
+      ],
+      instructions: [
+        'Misture o leite condensado, achocolatado e manteiga em uma panela.',
+        'Cozinhe em fogo baixo até dar o ponto de brigadeiro.',
+        'Enrole os brigadeiros e passe no granulado.'
+      ]
+    }
+  ];
+  
